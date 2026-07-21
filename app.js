7a0d8a6af635eba0439e5377220dbae5559429cb
Jeff Yankson <jjyankson19@gmail.com>
2026-07-21 01:11:27 +0000

    Encrypt secrets into dotenv file

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const controller = require('./controller/task.controller');
 const routes = require('./routes/task.routes');
-require('./models/task.model');
+const connectDB = require('./models/task.model');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,9 +12,19 @@ app.use(express.json());
 app.use('/api/v1/tasks', routes);
 
 app.get('/tasks');
-app.listen(PORT, () => {
-  console.log(
-    `Server is listening on Port: ${PORT}; http://localhost:${PORT}` +
-      ' Press Ctrl + C to cancel.',
-  );
-});
+
+async function init() {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(
+        `Server is listening on Port: ${PORT}; http://localhost:${PORT}` +
+          ' Press Ctrl + C to cancel.',
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
--- models/task.model.js
+++ models/task.model.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://jjyankson19_db_user:KQVPV7thWxlSRabM@clusterfreetier.oxfbhyt.mongodb.net/TaskManager?appName=ClusterFreeTier';
-
-const init = async () => {
-  try {
-    const response = await mongoose.connect(connectionString);
-    console.log('Connected to db successfully.');
-  } catch (error) {
-    console.log(error);
-  }
+const connectDB = async (url) => {
+  return mongoose.connect(url);
 };
 
-init();
+module.exports = connectDB

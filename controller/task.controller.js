4f8394d55f97f878568468d1ff1868a38ea5bb2a
Jeff Yankson <jjyankson19@gmail.com>
2026-07-24 16:13:38 +0000

    set up basic controller for task routes

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,5 +1,21 @@
 const getAllTasks = (req, res) => {
-  res.send('all items...')
+  res.send('all items from the file...');
 };
 
-module.exports = { getAllTasks };
+const createTask = (req, res) => {
+  res.send('all items from the file...');
+};
+
+const getTask = (req, res) => {
+  res.send('all items from the file...');
+};
+
+const updateTask = (req, res) => {
+  res.send('all items from the file...');
+};
+
+const deleteTask = (req, res) => {
+  res.send('all items from the file...');
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTasks } = require('../controller/task.controller');
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require('../controller/task.controller');
 
-router.route('/').get((req, res) => {
-  res.send('all items');
-});
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;

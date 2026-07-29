205d4dd6b89dcb9af8c510d130a983d0092833f4
Jeff Yankson <jjyankson19@gmail.com>
2026-07-29 12:27:34 +0000

    Set up connection to database

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import router from './routes/task.routes.js';
-import * as models from './models/connect.models.js';
+import connectDB from './models/connect.models.js';
 
 const app = express();
 
-// routes
-app.get('/hello', (req, res) => {
-  res.json({ msg: 'Hey hey' });
-});
-
 const PORT = process.env.PORT || 3000;
+
+const init = async (URL) => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(
+      `Server is running on http://localhost:${PORT}` +
+        '; Press Ctrl + C to terminate.',
+    );
+  });
+};
+
+init();
 
 // Set up middleware
 app.use(express.json());
 app.use('/api/v1/tasks', router);
-
-app.listen(PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${PORT}` +
-      '; Press Ctrl + C to terminate.',
-  );
-});
--- models/connect.models.js
+++ models/connect.models.js
@@ -3,7 +3,7 @@ import mongoose, { mongo } from 'mongoose';
 const connectionString =
   'mongodb+srv://jjyankson19_db_user:QVqVmo2MicVAnytU@task-manager-api.dwj8sm3.mongodb.net/task_manager_db';
 
-async function connectDB() {
+async function connectDB(url) {
   try {
     await mongoose.connect(connectionString);
     console.log('connected to db');
@@ -11,4 +11,5 @@ async function connectDB() {
     console.log(error);
   }
 }
-connectDB();
+
+export default connectDB;

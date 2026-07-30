8dd3e6aef6d86a27e2c9ea4cdc6bc0cada192114
Jeff Yankson <jjyankson19@gmail.com>
2026-07-30 11:06:23 +0000

    Rewrites code for the DELETE controller for better perfomance

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -33,6 +33,16 @@ export const updateTask = (req, res) => {
   res.send('update task');
 };
 
-export const deleteTask = (req, res) => {
-  res.send('delete task');
+export const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task: null, status: 'success' });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
